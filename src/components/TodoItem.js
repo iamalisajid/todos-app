cd0e6badbe594fe7b66d7cb60643f1df4f591419
Ali Sajid <imalisajid@gmail.com>
2018-10-20 16:48:53 +0500

    Initial commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TodoItem = (props) => {
+    return (
+
+    );
+}
+
+TodoItem.propTypes = {}
+
+export default TodoItem
---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class TodoList extends React.Component {
+
+    render() {
+
+    }
+
+}
+
+TodoList.propTypes = {};
+
+TodoList.defaultProps = {};
+
+export default TodoList;
