47d3988dab728db211d1db3a81da0cbb22227fda
Ali Sajid <imalisajid@gmail.com>
2018-10-22 15:11:15 +0500

    contacts page design

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import App from "./viewes/App";
 import Todos from "./viewes/Todos";
 import Contacts from "./viewes/Contacts";
+import Dashboard from "./viewes/Dashboard";
 
 export default (
   <Switch>
-    <Route path="/" exact component={App} />
+    <Route path="/" exact component={Dashboard}/>
     <Route path="/todos" component={Todos} />
     <Route path="/contacts" component={Contacts} />
   </Switch>
