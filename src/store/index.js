cbdff311a55279d1927739ba87e1310c436e0013
Ali Sajid <imalisajid@gmail.com>
2018-10-26 11:59:58 +0500

    added redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,16 @@
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
 const middlewares = [thunk];
+
 const getStore = initialState => createStore(
   rootReducer,
   initialState,
-  applyMiddleware(...middlewares)
+  composeWithDevTools(
+    applyMiddleware(...middlewares)
+  )
 );
 
 export default getStore;
