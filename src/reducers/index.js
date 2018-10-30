891028ff4b6ce7bcb7ef86d276a0f58d42545df4
Ali Sajid <imalisajid@gmail.com>
2018-10-30 12:18:44 +0500

    conditional rendering of logout button based on route

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,10 @@
 import {combineReducers} from 'redux';
-import {persistReducer} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import login from './login';
 import todos from './todos';
 import contacts from './contacts';
 
-const loginPersistConfig = {
-  key: 'login',
-  storage: storage,
-  blacklist: ['loading', 'error', 'loginForm']
-};
-
 const appReducer = combineReducers({
-  login: persistReducer(loginPersistConfig, login),
+  login,
   todos,
   contacts
 });
@@ -22,6 +14,6 @@ const rootReducer = (state, action) => {
     state = undefined
   }
   return appReducer(state, action)
-}
+};
 
 export default rootReducer;
--- src/viewes/App/Header/index.js
+++ src/viewes/App/Header/index.js
@@ -1,21 +1,29 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {object} from 'prop-types';
+import {withRouter} from 'react-router';
 import {HEADER_TITILE} from '../../../utils/copies';
-import {BTN_ACTIONS} from '../../../utils/constants';
+import {APP_ROUTES, BTN_ACTIONS} from '../../../utils/constants';
 
-const Header = () => (
+const Header = ({location}) => (
   <header className="App-header">
     <div className="row">
       <div className="col-lg-6">
-        <h2 className="App-title">{HEADER_TITILE}</h2>
+        <h2 className="App-title">{HEADER_TITILE}
+        </h2>
       </div>
       <div className="col-lg-6">
-        <Link className="btn btn-danger float-right" to="/">
+        {(location.pathname !== APP_ROUTES.BASE && location.pathname !== APP_ROUTES.LOGIN) &&
+        <Link className="btn btn-danger float-right" to={APP_ROUTES.LOGIN}>
           <span className="fa fa-sign-out"/>{BTN_ACTIONS.LOGOUT}
-        </Link>
+        </Link>}
       </div>
     </div>
   </header>
 );
 
-export default Header;
+Header.propTypes = {
+  location: object.isRequired
+};
+
+export default withRouter(Header);
