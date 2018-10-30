81fe568cab563f49c3277f5cca089c14d536c3b4
Ali Sajid <imalisajid@gmail.com>
2018-10-30 15:32:40 +0500

    added precommit hooks

--- src/viewes/Todos/index.js
+++ src/viewes/Todos/index.js
@@ -1,11 +1,12 @@
-import React, {Component, Fragment} from 'react';
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
+import React, { Component, Fragment } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { object } from 'prop-types';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm';
-import TodoFilter from "./TodoFilter";
+import TodoFilter from './TodoFilter';
 import AppLoader from '../../shared/loader';
-import {FILTERS} from '../../utils/constants';
+import { FILTERS } from '../../utils/constants';
 import * as todoActions from './actions';
 
 class Todos extends Component {
@@ -13,42 +14,44 @@ class Todos extends Component {
     this.props.actions.fetchTodos();
   }
 
-  handleInput = event => {
-    let todoForm = event.target.value;
+  handleInput = (event) => {
+    const todoForm = event.target.value;
     this.props.actions.updateTodoField(todoForm);
   };
 
-  handleSubmit = event => {
+  handleSubmit = (event) => {
     event.preventDefault();
-    this.props.actions.createTodo({text: this.props.todoForm, isComplete: false});
+    this.props.actions.createTodo({
+      text: this.props.todoForm,
+      isComplete: false
+    });
   };
 
-  toggleTodo = event => {
-    const {todos} = this.props;
+  toggleTodo = (event) => {
+    const { todos } = this.props;
 
     const id = parseInt(event.target.value);
-    const todo = todos.find(todo => todo.id === id);
-    const toggled = {...todo, isComplete: !todo.isComplete};
+    const todo = todos.find((todo) => todo.id === id);
+    const toggled = { ...todo, isComplete: !todo.isComplete };
 
     this.props.actions.toggleTodo(toggled);
   };
 
-  handleDelete = event => {
+  handleDelete = (event) => {
     event.stopPropagation();
     const id = parseInt(event.target.value);
-    this.props.actions.deleteTodo(id)
+    this.props.actions.deleteTodo(id);
   };
 
-  handleFilter = event => {
+  handleFilter = (event) => {
     const filter = event.target.value;
-    this.props.actions.updateTodosFilter(filter)
+    this.props.actions.updateTodosFilter(filter);
   };
 
   render() {
-    const {todos, loading, todoForm, error} = this.props;
+    const { todos, loading, todoForm, error } = this.props;
 
-    if (loading)
-      return <AppLoader/>;
+    if (loading) return <AppLoader/>;
 
     return (
       <Fragment>
@@ -63,38 +66,36 @@ class Todos extends Component {
           toggleTodo={this.toggleTodo}
           handleDelete={this.handleDelete}
         />
-        <TodoFilter
-          handleFilter={this.handleFilter}
-        />
+        <TodoFilter handleFilter={this.handleFilter}/>
       </Fragment>
     );
   }
 }
-
 const visibleTodos = (todos, filter = FILTERS.SHOW_ALL) => {
   switch (filter) {
     case FILTERS.SHOW_ALL:
       return todos;
     case FILTERS.SHOW_ACTIVE:
-      return todos.filter(todo => !todo.isComplete);
+      return todos.filter((todo) => !todo.isComplete);
     case FILTERS.SHOW_COMPLETED:
-      return todos.filter(todo => todo.isComplete);
+      return todos.filter((todo) => todo.isComplete);
     default:
       return todos;
   }
 };
 
-const mapStateToProps = ({todos}) => ({
-    todoForm: todos.todoForm,
-    todos: visibleTodos(todos.todos, todos.filter),
-    loading: todos.loading,
-    error: todos.error
-  }
-);
+const mapStateToProps = ({ todos }) => ({
+  todoForm: todos.todoForm,
+  todos: visibleTodos(todos.todos, todos.filter),
+  loading: todos.loading,
+  error: todos.error
+});
 
-const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators(todoActions, dispatch)
-  }
-);
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(todoActions, dispatch)
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Todos);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Todos);
