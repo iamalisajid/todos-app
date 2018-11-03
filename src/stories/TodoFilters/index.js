8d7c260309a0ff29ead72e5f6cf85adcecbbd91b
Ali Sajid <AliSajid@MacBook-Pro-2.local>
2018-11-03 11:52:28 +0500

    added stories for filter,list and screen

---
+++ src/stories/TodoFilters/index.js
@@ -0,0 +1,36 @@
+import React, { Fragment } from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import TodoList from '../../viewes/Todos/TodoList';
+import TodoFilter from '../../viewes/Todos/TodoFilter';
+
+export const todos = [
+  { text: 'Sample Todo', isComplete: false, id: 3 },
+  { text: 'New Todo', isComplete: false, id: 2 },
+  { text: 'Done Todo', isComplete: true, id: 1 },
+];
+
+export const actions = {
+  toggleTodo: action('toggleTodo'),
+  handleDelete: action('handleDelete'),
+};
+
+storiesOf('TodoFilter', module)
+  .add('default', () => (
+    <Fragment>
+      <TodoList todos={todos} {...actions} />
+      <TodoFilter />
+    </Fragment>
+  ))
+  .add('active', () => (
+    <Fragment>
+      <TodoList todos={todos.slice(0, 1)} {...actions} />
+      <TodoFilter />
+    </Fragment>
+  ))
+  .add('completed', () => (
+    <Fragment>
+      <TodoList todos={todos.splice(-1, 1)} {...actions} />
+      <TodoFilter />
+    </Fragment>
+  ));
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,19 +1,4 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import TodoItem from '../viewes/Todos/TodoItem';
-
-export const todo = {
-  text: 'Sample Todo',
-  isComplete: false,
-  id: 3
-};
-
-export const actions = {
-  toggleTodo: action('toggleTodo'),
-  handleDelete: action('handleDelete')
-};
-
-storiesOf('TodoItem', module)
-  .add('default', () => <TodoItem {...todo} {...actions}/>)
-  .add('completed', () => <TodoItem id={todo.id} text={todo.text} isComplete={!todo.isComplete} {...actions}/>);
+export * from './TodoItem';
+export * from './TodoList';
+export * from './TodoFilters';
+export * from './TodoScreen';
